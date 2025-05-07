6af8420f69f4c3f30af052510fb8f025c0ea3cb0
SaFisch <sandra.fischer01@sap.com>
2025-05-07 13:44:57 +0200

    Update main.js

--- main.js
+++ main.js
@@ -125,6 +125,9 @@
       const [day, month, year] = datePart.split(".").map(Number);
       const [hours, minutes, seconds] = timePart.split(":").map(Number);
       return new Date(year, month - 1, day, hours, minutes, seconds);
+       console.log(`Parsed Datum: ${dateString} -> ${parsedDate}`);
+  
+     return parsedDate;
     }
 
      async render() {
